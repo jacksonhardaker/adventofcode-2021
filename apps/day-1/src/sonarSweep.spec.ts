a24d8863d3a881ce64722e1d80ea7017624c25e0
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-01 06:55:05 -0500

    add initial test

--- apps/day-1/src/sonarSweep.spec.ts
+++ apps/day-1/src/sonarSweep.spec.ts
@@ -1,8 +1,19 @@
 import { sonarSweep } from './sonarSweep';
 
 test('should do something cool', () => {
-  const input = [];
+  const input = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263,
+  ];
   const result = sonarSweep(input);
 
-  expect(result).toEqual(null);
+  expect(result).toEqual(7);
 });
