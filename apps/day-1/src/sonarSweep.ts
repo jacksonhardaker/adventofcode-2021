0697dfa1bf4db43fe9b3d6bfe1f9bc0d618b3891
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-01 07:25:23 -0500

    refactor slidingWindow solution

--- apps/day-1/src/sonarSweep.ts
+++ apps/day-1/src/sonarSweep.ts
@@ -5,20 +5,14 @@ export const sonarSweep = (input: number[]) => {
   );
 };
 
-const getWindow = (index: number, input: number[]) =>
-  input[index] + input[index + 1] + input[index + 2];
-
 export const slidingWindow = (input: number[]) => {
-  let increaseCount = 0;
-  for (let i = 0; i < input.length - 3; i++) {
-    const currentWindow = getWindow(i, input);
-    const nextWindow = getWindow(i + 1, input);
 
-    if (Number.isNaN(currentWindow) || Number.isNaN(nextWindow)) break;
+  const getWindow = (i: number) => input[i] + input[i + 1] + input[i + 2];
 
-    if (nextWindow > currentWindow) {
-      increaseCount++;
-    }
-  }
-  return increaseCount;
+  return input.reduce((count, _, i) => {
+    const currentWindow = getWindow(i);
+    const nextWindow = getWindow(i + 1);
+
+    return !Number.isNaN(nextWindow) && nextWindow > currentWindow ? count + 1 : count;
+  }, 0);
 };
