1aa2a952dc4f6175eddfebf2d5b1e2e6ae9875f1
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-03 08:04:30 -0500

    complete part 2

--- apps/day-3/src/main.ts
+++ apps/day-3/src/main.ts
@@ -1,9 +1,11 @@
 import { processInput } from '@adventofcode-2021/util-io';
-import { binaryDiagnostic } from './binaryDiagnostic';
+import { binaryDiagnostic, oxygenDiagnostic } from './binaryDiagnostic';
 
 (async () => {
   const input = await processInput('assets/input.txt', { root: __dirname });
   const result1 = binaryDiagnostic(input);
+  const result2 = oxygenDiagnostic(input);
 
   console.log(`part 1: ${result1}`);
+  console.log(`part 2: ${result2}`);
 })();
