be2176e4200748838d3d2ccdc842cb0990edb6c0
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-03 06:56:31 -0500

    add solution for part 1

--- apps/day-3/src/binaryDiagnostic.spec.ts
+++ apps/day-3/src/binaryDiagnostic.spec.ts
@@ -2,9 +2,22 @@ import { binaryDiagnostic } from './binaryDiagnostic';
 
 describe('binaryDiagnostic', () => {
   test('should return the expected result', () => {
-    const input = [];
+    const input = [
+      '00100',
+      '11110',
+      '10110',
+      '10111',
+      '10101',
+      '01111',
+      '00111',
+      '11100',
+      '10000',
+      '11001',
+      '00010',
+      '01010',
+    ];
     const result = binaryDiagnostic(input);
 
-    expect(result).toEqual(null);
+    expect(result).toEqual(198);
   });
 });
--- apps/day-3/src/binaryDiagnostic.ts
+++ apps/day-3/src/binaryDiagnostic.ts
@@ -1,3 +1,20 @@
-export const binaryDiagnostic = (input: any[]) => {
-  return null;
+export const binaryDiagnostic = (input: string[]) => {
+  const stats = input.reduce((acc, binary) => {
+    const stats = { ...acc };
+    binary.split('').forEach((bit, index) => {
+      if (!stats[index]) stats[index] = {};
+
+      stats[index][bit] = stats[index][bit] ? stats[index][bit] + 1 : 1
+
+      stats[index].gammaRate = stats[index]['1'] > stats[index]['0'] ? '1' : '0';
+      stats[index].epsilonRate = stats[index]['1'] < stats[index]['0'] ? '1' : '0';
+    });
+
+    return stats;
+  }, {});
+
+  const gammaRate = Number.parseInt(Object.values(stats).map(({ gammaRate }) => gammaRate).join(''), 2);
+  const epsilonRate = Number.parseInt(Object.values(stats).map(({ epsilonRate }) => epsilonRate).join(''), 2);
+
+  return gammaRate * epsilonRate;
 };
