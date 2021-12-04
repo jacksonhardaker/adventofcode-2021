100393c5602a1631b8d34cd440564148eed2131f
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-04 07:58:57 -0500

    add solution to part 1

--- apps/day-4/src/giantSquid.ts
+++ apps/day-4/src/giantSquid.ts
@@ -1,19 +1,59 @@
+type Row = number[];
+type Board = Row[];
+
 export const parseInput = (input: string) => {
   const [orderString, ...rest] = input.split('\n');
   const numbers = orderString.split(',').map(Number);
 
-  const boards = rest.reduce((acc, row) => {
+  const boards: Board[] = rest.reduce((acc, row) => {
     row.length === 0
       ? acc.push([])
       : acc[acc.length - 1].push(row.match(/(\d+)/g).map(Number));
     return acc;
   }, []);
 
-  return [numbers, boards];
+  return [numbers, boards] as [number[], Board[]];
+};
+
+const findWinningBoard = (numbers: number[], boards: Board[]) => {
+  const calledNumbers = new Map();
+
+  for (const number of numbers) {
+    calledNumbers.set(number, true);
+
+    for (const board of boards) {
+      for (const row of board) {
+        const isWinningRow = row.every((num) => calledNumbers.get(num));
+
+        if (isWinningRow) {
+          return [board, calledNumbers] as [Board, Map<number, boolean>];
+        }
+      }
+
+      for (let col = 0; col < 5; col++) {
+        const isWinningCol = board.every((row) => calledNumbers.get(row[col]));
+
+        if (isWinningCol) {
+          return [board, calledNumbers] as [Board, Map<number, boolean>];
+        }
+      }
+    }
+  }
 };
 
 export const giantSquid = (input: string) => {
-  parseInput(input);
+  const [numbers, boards] = parseInput(input);
 
-  return null;
+  const [winningBoard, calledNumbers] = findWinningBoard(numbers, boards);
+
+  const lastNumber: number = Array.from(calledNumbers.keys()).pop();
+
+  const sumOfUncalled = winningBoard.reduce((sum, row) => {
+    row.forEach((num) => {
+      sum = calledNumbers.get(num) ? sum : sum + num;
+    });
+    return sum;
+  }, 0);
+
+  return sumOfUncalled * lastNumber;
 };
