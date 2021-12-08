5093b57a269f51353aed716cb9fec74f6e00a9f7
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-08 06:37:19 -0500

    add solution to part 1

--- apps/day-8/src/sevenSegmentSearch.ts
+++ apps/day-8/src/sevenSegmentSearch.ts
@@ -1,3 +1,14 @@
-export const sevenSegmentSearch = (input: any[]) => {
-  return null;
+
+export const sevenSegmentSearch = (input: string[]) => {
+  return input.reduce((acc, input) => {
+    const matches = input
+      .split('|')[1]
+      .match(
+        RegExp(
+          [2, 3, 4, 7].map((num) => `(\\b[a-z]{${num}}\\b)`).join('|'),
+          'g'
+        )
+      ) || [];
+    return matches.length + acc;
+  }, 0);
 };
