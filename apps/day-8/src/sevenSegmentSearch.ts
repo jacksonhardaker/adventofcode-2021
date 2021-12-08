e6afcbcd5bd5ebfefd8df1d876e07ab26f304ab2
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-08 09:20:36 -0500

    pre sort patterns

--- apps/day-8/src/sevenSegmentSearch.ts
+++ apps/day-8/src/sevenSegmentSearch.ts
@@ -15,43 +15,42 @@ export const sevenSegmentSearch = (input: string[]) => {
 
 export const part2 = (input: string[]) => {
   const parsedInput = input.map((entry) => {
-    const matches = entry.match(/[a-z]{2,7}/g);
+    const matches = entry
+      .match(/[a-z]{2,7}/g)
+      .map((p) => Array.from(p).sort().join(''));
     const patterns = matches.slice(0, 10);
     const values = matches.slice(10, 14);
 
     return [patterns, values];
   });
 
-  const equals = (p1, p2) =>
-    p1.length === p2.length && Array.from(p1).every((c) => p2.includes(c));
-
   const includesAllFrom = (from, p) =>
     Array.from(from).every((c) => p.includes(c));
 
-  const hasLength = (p, length) => p.length === length;
+  const hasLengthN = (length) => (p) => p.length === length;
 
   return parsedInput.reduce((sum, [patterns, values]) => {
     const [one, four, seven, eight] = [2, 4, 3, 7].map((length) =>
-      patterns.find((p) => p.length === length)
+      patterns.find(hasLengthN(length))
     );
 
     // if 5 chars AND includes ALL from 1 === 3
     const three = patterns.find(
-      (p) => hasLength(p, 5) && includesAllFrom(one, p)
+      (p) => hasLengthN(5)(p) && includesAllFrom(one, p)
     );
 
     // if 6 chars AND includes ALL from 4 === 9
     const nine = patterns.find(
-      (p) => hasLength(p, 6) && includesAllFrom(four, p)
+      (p) => hasLengthN(6)(p) && includesAllFrom(four, p)
     );
 
     // if 6 chars AND includes ALL but 1 from 9 AND the letter NOT in 9 exists in all 5 char patterns === 0
     const zero = patterns.find(
       (p) =>
-        hasLength(p, 6) &&
-        !equals(p, nine) &&
+        hasLengthN(6)(p) &&
+        p !== nine &&
         patterns
-          .filter((p2) => hasLength(p2, 5))
+          .filter((p2) => hasLengthN(5)(p2))
           .every((p3) =>
             p3.includes(Array.from(nine).find((c) => !p.includes(c)))
           )
@@ -59,38 +58,37 @@ export const part2 = (input: string[]) => {
 
     // if 6 chars AND is NOT 9 or 0 === 6
     const six = patterns.find(
-      (p) => hasLength(p, 6) && !equals(p, nine) && !equals(p, zero)
+      (p) => hasLengthN(6)(p) && p !== nine && p !== zero
     );
 
     // if 5 chars AND 9 includes all chars AND is not 3 === 5
     const five = patterns.find(
-      (p) => hasLength(p, 5) && includesAllFrom(p, nine) && !equals(p, three)
+      (p) => hasLengthN(5)(p) && includesAllFrom(p, nine) && p !== three
     );
 
     // if 5 chars AND is NOT 3 or 5 === 2
     const two = patterns.find(
-      (p) => hasLength(p, 5) && !equals(p, three) && !equals(p, five)
+      (p) => hasLengthN(5)(p) && p !== three && p !== five
     );
 
-    const numbers = [
-      zero,
-      one,
-      two,
-      three,
-      four,
-      five,
-      six,
-      seven,
-      eight,
-      nine,
-    ];
-
     const value = Number(
-      values
-        .map((value) => {
-          return numbers.findIndex((p) => equals(p, value));
-        })
-        .join('')
+      values.reduce(
+        (num, val) =>
+          num +
+          [
+            zero,
+            one,
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine,
+          ].findIndex((p) => p === val),
+        ''
+      )
     );
 
     return (sum += value);
