a9a5f2c2fcc0514ce6608fb5219e360ed0469b9c
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-09 06:52:58 -0500

    day 9 (#10)
    * generate day 9
    
    * add readme and input
    
    * add solution to part 1
    
    * add part 2 to readme
    
    * add solution for part 2

---
+++ apps/day-9/src/smokeBasin.spec.ts
@@ -0,0 +1,29 @@
+import { smokeBasin, part2 } from './smokeBasin';
+
+describe('smokeBasin', () => {
+  test('should return the expected result', () => {
+    const input = [
+      [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+      [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+      [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+      [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+      [9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+    ];
+    const result = smokeBasin(input);
+
+    expect(result).toEqual(15);
+  });
+
+  test('should return the expected result', () => {
+    const input = [
+      [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+      [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+      [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+      [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+      [9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+    ];
+    const result = part2(input);
+
+    expect(result).toEqual(1134);
+  });
+});
---
+++ apps/day-9/src/smokeBasin.ts
@@ -0,0 +1,49 @@
+const findLowPoints = (input: number[][]) => {
+  const lowPoints = [];
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      const surrounding = [
+        [y - 1, x],
+        [y + 1, x],
+        [y, x - 1],
+        [y, x + 1],
+      ].map(([yy, xx]) => input?.[yy]?.[xx] ?? Infinity);
+
+      if (surrounding.every((point) => input[y][x] < point)) {
+        lowPoints.push([x, y]);
+      }
+    }
+  }
+  return lowPoints;
+};
+
+export const smokeBasin = (input: number[][]) => {
+  const lowPoints = findLowPoints(input);
+  return lowPoints.reduce((acc, [x, y]) => acc + input[y][x] + 1, 0);
+};
+
+export const part2 = (input: number[][]) => {
+  const lowPoints = findLowPoints(input);
+
+  const traverse = (x, y, visited = {}) => {
+    const point = input?.[y]?.[x];
+    visited[`${x},${y}`] = true;
+
+    const visit = (x2, y2) =>
+      visited[`${x2},${y2}`] ? 0 : traverse(x2, y2, visited);
+
+    if (point === 9 || point === undefined) return 0;
+
+    return (
+      1 + visit(x - 1, y) + visit(x + 1, y) + visit(x, y - 1) + visit(x, y + 1)
+    );
+  };
+
+  const basinSizes = lowPoints.map(([x, y]) => traverse(x, y));
+  const productOf3Largest = basinSizes
+    .sort((a, b) => (a === b ? 0 : a > b ? 1 : -1))
+    .slice(-3)
+    .reduce((acc, size) => acc * size, 1);
+
+  return productOf3Largest;
+};
