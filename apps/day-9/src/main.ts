31035af20789250afcab776e231a0f340bf44d03
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-09 06:29:59 -0500

    add solution to part 1

--- apps/day-9/src/main.ts
+++ apps/day-9/src/main.ts
@@ -2,7 +2,7 @@ import { processInput } from '@adventofcode-2021/util-io';
 import { smokeBasin } from './smokeBasin';
 
 (async () => {
-  const input = await processInput('assets/input.txt', { root: __dirname });
+  const input = await processInput('assets/input.txt', { root: __dirname, parser: (line) => Array.from(line).map(Number) });
   const result1 = smokeBasin(input);
 
   console.log(`part 1: ${result1}`);
--- apps/day-9/src/smokeBasin.ts
+++ apps/day-9/src/smokeBasin.ts
@@ -1,3 +1,19 @@
-export const smokeBasin = (input: any[]) => {
-  return null;
+export const smokeBasin = (input: number[][]) => {
+  const lowPoints = [];
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      const surrounding = [
+        [y - 1, x],
+        [y + 1, x],
+        [y, x - 1],
+        [y, x + 1],
+      ].map(([yy, xx]) => input?.[yy]?.[xx] ?? Infinity);
+
+      if (surrounding.every((point) => input[y][x] < point)) {
+        lowPoints.push([x, y]);
+      }
+    }
+  }
+
+  return lowPoints.reduce((acc, [x,y]) => acc + input[y][x] + 1, 0);
 };
