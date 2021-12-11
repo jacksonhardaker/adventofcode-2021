d31f294fad0b4102b16c38025b89e99fb791c917
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-11 08:47:36 -0500

    add parser to main

--- apps/day-11/src/main.ts
+++ apps/day-11/src/main.ts
@@ -2,7 +2,7 @@ import { processInput } from '@adventofcode-2021/util-io';
 import { dumbOctopus } from './dumbOctopus';
 
 (async () => {
-  const input = await processInput('assets/input.txt', { root: __dirname });
+  const input = await processInput('assets/input.txt', { root: __dirname, parser: (line) => Array.from(line).map(Number) });
   const result1 = dumbOctopus(input);
 
   console.log(`part 1: ${result1}`);
