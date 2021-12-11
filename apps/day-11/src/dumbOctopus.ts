43d1aab4768d864f9798e2be5a16454cd7c98ee5
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-11 11:16:04 -0500

    optimize day 11 a bit

--- apps/day-11/src/dumbOctopus.ts
+++ apps/day-11/src/dumbOctopus.ts
@@ -1,76 +1,65 @@
+const surroundingPoints = (y, x) =>
+  Array(3)
+    .fill(0)
+    .flatMap((_, i) =>
+      Array(3)
+        .fill(0)
+        .map((_, j) => [i + y - 1, j + x - 1])
+    );
+
 export const takeSteps = (input: number[][], steps: number) => {
   const octopuses = input.map((row) => [...row]);
-  let flashCount = 0;
-  let allFlashed = false;
-  // find 9s
+  const flashCount = [];
+
   const takeStep = () => {
     const hasFlashed = {};
-    let nines = {};
+    let flashed: { [key: string]: [number, number] } = {};
+
+    const handlePoint = (y, x) => {
+      if (octopuses?.[y]?.[x] === 9) {
+        flashed[`${y},${x}`] = [y, x];
+        hasFlashed[`${y},${x}`] = true;
+        octopuses[y][x] = 0;
+      } else if (
+        octopuses?.[y]?.[x] !== undefined &&
+        !hasFlashed[`${y},${x}`]
+      ) {
+        octopuses[y][x]++;
+      }
+    };
+
     for (let y = 0; y < octopuses.length; y++) {
       for (let x = 0; x < octopuses[y].length; x++) {
-        const oct = octopuses[y][x];
-        if (oct === 9 && !hasFlashed[`${y},${x}`]) {
-          nines[`${y},${x}`] = [y, x];
-          hasFlashed[`${y},${x}`] = true;
-          octopuses[y][x] = 0;
-        } else if (oct < 9 && !hasFlashed[`${y},${x}`]) {
-          octopuses[y][x]++;
-        }
+        handlePoint(y, x);
       }
     }
 
-    while (Object.keys(nines).length > 0) {
-      const justFlashed = { ...nines };
-      nines = {};
+    while (Object.keys(flashed).length > 0) {
+      const justFlashed = { ...flashed };
+      flashed = {};
       for (const key in justFlashed) {
-        const [y, x] = justFlashed[key];
-        [
-          [y - 1, x - 1],
-          [y - 1, x],
-          [y - 1, x + 1],
-          [y, x - 1],
-          [y, x],
-          [y, x + 1],
-          [y + 1, x - 1],
-          [y + 1, x],
-          [y + 1, x + 1],
-        ].forEach(([y2, x2]) => {
-          if (octopuses?.[y2]?.[x2] === 9) {
-            octopuses[y2][x2] = 0;
-            hasFlashed[`${y2},${x2}`] = true;
-            nines[`${y2},${x2}`] = [y2, x2];
-          } else if (
-            octopuses?.[y2]?.[x2] !== undefined &&
-            !hasFlashed[`${y2},${x2}`]
-          ) {
-            octopuses[y2][x2]++;
-          }
+        surroundingPoints(...justFlashed[key]).forEach(([y2, x2]) => {
+          handlePoint(y2, x2);
         });
       }
     }
 
-    if (
-      Object.keys(hasFlashed).length ===
-      octopuses.length * octopuses[0].length
-    ) {
-      allFlashed = true;
-    }
-
-    flashCount += Object.keys(hasFlashed).length;
+    flashCount.push(Object.keys(hasFlashed).length);
   };
 
   for (let i = 0; i < steps; i++) {
     takeStep();
   }
 
-  return { octopuses, flashCount, allFlashed };
+  return {
+    octopuses,
+    flashCount,
+    allFlashed: flashCount.slice(-1)[0] === 100,
+  };
 };
 
-export const dumbOctopus = (input: number[][], steps = 100) => {
-  const { flashCount } = takeSteps(input, steps);
-
-  return flashCount;
-};
+export const dumbOctopus = (input: number[][], steps = 100) =>
+  takeSteps(input, steps).flashCount.reduce((acc, val) => acc + val, 0);
 
 export const dumbOctopusSync = (input: number[][]) => {
   let results = takeSteps(input, 1);
