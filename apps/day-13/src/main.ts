de5dc8ea8a445c8ecba6f6469b2d20a9f0e35ed8
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-13 07:18:58 -0500

    print part two in main

--- apps/day-13/src/main.ts
+++ apps/day-13/src/main.ts
@@ -1,5 +1,5 @@
 import { processInput } from '@adventofcode-2021/util-io';
-import { transparentOrigami } from './transparentOrigami';
+import { printInstructions, transparentOrigami } from './transparentOrigami';
 
 (async () => {
   const input = await processInput('assets/input.txt', {
@@ -8,6 +8,8 @@ import { transparentOrigami } from './transparentOrigami';
     parser: (line) => line.split('\n'),
   });
   const result1 = transparentOrigami(input);
-
+  
   console.log(`part 1: ${result1}`);
+  console.log('part 2:');
+  printInstructions(input);
 })();
