8ed0ed40e4fa46f051809322b0728b40dfdb0ccf
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-13 07:28:17 -0500

    refactor to remove if/else

--- apps/day-13/src/transparentOrigami.ts
+++ apps/day-13/src/transparentOrigami.ts
@@ -1,17 +1,17 @@
 type Fold = { axis: 'y' | 'x'; line: number };
 type Dot = { x: number; y: number };
 
-const parseInput = (input: string[][]) => {
-  const dots = input[0].map((line) => {
-    const [x, y] = line.split(',').map(Number);
-    return { x, y } as Dot;
-  });
-  const folds = input[1].map((line) => {
-    const [, axis, fold] = line.match(/fold\salong\s(y|x)=(\d+)/);
-    return { axis, line: Number(fold) } as Fold;
-  });
-  return [dots, folds] as [Dot[], Fold[]];
-};
+const parseInput = (input: string[][]) =>
+  [
+    input[0].map((line) => {
+      const [x, y] = line.split(',').map(Number);
+      return { x, y } as Dot;
+    }),
+    input[1].map((line) => {
+      const [, axis, fold] = line.match(/fold\salong\s(y|x)=(\d+)/);
+      return { axis, line: Number(fold) } as Fold;
+    }),
+  ] as [Dot[], Fold[]];
 
 const printDots = (dots: Dot[]) => {
   const output = dots.reduce((output, { x, y }) => {
@@ -20,30 +20,29 @@ const printDots = (dots: Dot[]) => {
     return output;
   }, []);
 
-  const outputStr = output
-    .map((row) => {
-      let rowStr = '';
-      for (const point of row) {
-        rowStr += point || '.';
-      }
-      return rowStr;
-    })
-    .join('\n');
-
-  console.log(outputStr);
+  console.log(
+    output
+      .map((row) => {
+        let rowStr = '';
+        for (const point of row) {
+          rowStr += point || '.';
+        }
+        return rowStr;
+      })
+      .join('\n')
+  );
 };
 
 const applyFold = (dots: Dot[], fold: Fold) => {
   const foldedDots = dots.reduce((acc, dot) => {
-    if (dot[fold.axis] < fold.line) {
-      return { ...acc, [`${dot.x},${dot.y}`]: dot };
-    } else {
-      const dot2 = {
-        ...dot,
-        [fold.axis]: Math.abs(dot[fold.axis] - 2 * fold.line),
-      };
-      return { ...acc, [`${dot2.x},${dot2.y}`]: dot2 };
-    }
+    const dot2 =
+      dot[fold.axis] < fold.line
+        ? dot
+        : {
+            ...dot,
+            [fold.axis]: Math.abs(dot[fold.axis] - 2 * fold.line),
+          };
+    return { ...acc, [`${dot2.x},${dot2.y}`]: dot2 };
   }, {});
 
   return Object.values(foldedDots) as Dot[];
@@ -51,7 +50,6 @@ const applyFold = (dots: Dot[], fold: Fold) => {
 
 export const transparentOrigami = (input: string[][], foldsToExec = 1) => {
   const [dots, folds] = parseInput(input);
-
   return folds
     .slice(0, foldsToExec)
     .reduce((acc, fold) => applyFold(acc, fold), dots).length;
@@ -59,7 +57,6 @@ export const transparentOrigami = (input: string[][], foldsToExec = 1) => {
 
 export const printInstructions = (input: string[][]) => {
   const [dots, folds] = parseInput(input);
-
   const result = folds.reduce((acc, fold) => applyFold(acc, fold), dots);
   printDots(result);
 };
