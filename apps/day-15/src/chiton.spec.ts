ad0c633a1a870a3ceaeecd9296367f4ba37f6820
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-15 06:42:49 -0500

    generate day

---
+++ apps/day-15/src/chiton.spec.ts
@@ -0,0 +1,10 @@
+import { chiton } from './chiton';
+
+describe('chiton', () => {
+  test('should return the expected result', () => {
+    const input = [];
+    const result = chiton(input);
+
+    expect(result).toEqual(null);
+  });
+});
---
+++ apps/day-15/src/chiton.ts
@@ -0,0 +1,3 @@
+export const chiton = (input: any[]) => {
+  return null;
+};
