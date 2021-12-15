363eaaa02e507ae8b58e8c164656657ea9dd72a4
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-14 20:49:46 -0500

    simplify aggregation

--- apps/day-14/src/main.ts
+++ apps/day-14/src/main.ts
@@ -1,11 +1,13 @@
 import { processInput } from '@adventofcode-2021/util-io';
-import { extendedPolymerization } from './extendedPolymerizationV2';
+import { extendedPolymerization } from './extendedPolymerization';
+import { extendedPolymerization as extendedPolymerizationV2 } from './extendedPolymerizationV2';
 
 (async () => {
   const input = await processInput('assets/input.txt', { root: __dirname, delimiter: '\n\n' });
-  const result1 = extendedPolymerization(input);
-  const result2 = extendedPolymerization(input, 40);
+  const result1a = extendedPolymerization(input);
+  const result1b = extendedPolymerizationV2(input);
+  // const result2 = extendedPolymerization(input, 40);
 
-  console.log(`part 1: ${result1}`); // 3306
-  console.log(`part 2: ${result2}`);
+  console.log(`part 1: ${result1b}`); // 3306
+  // console.log(`part 2: ${result2}`);
 })();
