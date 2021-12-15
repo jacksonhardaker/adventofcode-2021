a3fd5f52a5742ec9163a64f56a19633431fc1180
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-15 12:14:21 -0500

    add performance tracking

--- apps/day-15/src/main.ts
+++ apps/day-15/src/main.ts
@@ -6,11 +6,15 @@ import { chiton, expandMap } from './chiton';
     root: __dirname,
     parser: (row) => Array.from(row).map(Number),
   });
+  const part1Start = performance.now();
   const result1 = chiton(input);
+  const part1End = performance.now();
   const expandedMap = expandMap(input);
   
-  console.log(`part 1: ${result1}`);
+  console.log(`part 1: ${result1}\nexecuted in: ${Math.round(part1End - part1Start)}ms`);
   
+  const part2Start = performance.now();
   const result2 = chiton(expandedMap);
-  console.log(`part 2: ${result2}`);
+  const part2End = performance.now();
+  console.log(`part 2: ${result2}\nexecuted in: ${Math.round(part2End - part2Start)}ms`);
 })();
