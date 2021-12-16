ada214ce5f28c334d2831e2b733eddf38fb103b9
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-16 09:19:34 -0500

    log result of part 2

--- apps/day-16/src/main.ts
+++ apps/day-16/src/main.ts
@@ -1,9 +1,11 @@
 import { processInput } from '@adventofcode-2021/util-io';
-import { sumOfVersions, packetDecoder } from './packetDecoder';
+import { sumOfVersions, packetDecoder, evalPackets } from './packetDecoder';
 
 (async () => {
   const [input] = await processInput('assets/input.txt', { root: __dirname });
   const result1 = sumOfVersions(packetDecoder(input));
+  const result2 = evalPackets(input);
 
   console.log(`part 1: ${result1}`);
+  console.log(`part 2: ${result2}`);
 })();
