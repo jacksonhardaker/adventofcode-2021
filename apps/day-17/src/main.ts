24f1a19aea556cb7fcaab032bd3aebf8a8e82972
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-17 07:13:48 -0500

    generate day 17 and add readme and input

---
+++ apps/day-17/src/main.ts
@@ -0,0 +1,9 @@
+import { processInput } from '@adventofcode-2021/util-io';
+import { trickShot } from './trickShot';
+
+(async () => {
+  const input = await processInput('assets/input.txt', { root: __dirname });
+  const result1 = trickShot(input);
+
+  console.log(`part 1: ${result1}`);
+})();
---
+++ apps/day-17/src/trickShot.spec.ts
@@ -0,0 +1,10 @@
+import { trickShot } from './trickShot';
+
+describe('trickShot', () => {
+  test('should return the expected result', () => {
+    const input = [];
+    const result = trickShot(input);
+
+    expect(result).toEqual(null);
+  });
+});
