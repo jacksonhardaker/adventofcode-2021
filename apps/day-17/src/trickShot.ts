865ed536be07eabcf820665e696ebf6e0c58138c
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-17 08:12:01 -0500

    add function to find valid trajectory

--- apps/day-17/src/trickShot.ts
+++ apps/day-17/src/trickShot.ts
@@ -24,20 +24,58 @@ export const parseInput = (input: string): TargetArea => {
  *    greater than 0, increases by 1 if it is less than 0, or does not change if it is already 0.
  * Due to gravity, the probe's y velocity decreases by 1.
  */
-const findTrajectories = (targetX, targetY) => {
-  const [startX, startY] = [0, 0];
-  const [minX, minY, maxX, maxY] = [-100, -100, 100, 100];
-};
+// const findTrajectories = (targetX, targetY) => {
+//   const [startX, startY] = [0, 0];
+//   const [minX, minY, maxX, maxY] = [-100, -100, 100, 100];
+// };
 
 export const trickShot = ({ x1, x2, y1, y2 }: TargetArea) => {
-  const cont = (delta, start, end) =>
-    start < end ? delta <= end : delta >= end;
-  const inc = (start, end) => (start < end ? 1 : -1);
+  const [startX, startY] = [0, 0];
+  // const cont = (delta, start, end) =>
+  //   start < end ? delta <= end : delta >= end;
+  // const inc = (start, end) => (start < end ? 1 : -1);
 
-  for (let x = x1; cont(x, x1, x2); x += inc(x1, x2)) {
-    for (let y = y1; cont(y, y1, y2); y += inc(y1, y2)) {
-      // find all trajectories which will hit x,y from a start of 0,0
+  // for (let x = x1; cont(x, x1, x2); x += inc(x1, x2)) {
+  //   for (let y = y1; cont(y, y1, y2); y += inc(y1, y2)) {
+  //     // find all trajectories which will hit x,y from a start of 0,0
+  //   }
+  // }
+  const willHitTarget = ([initialVelX, initialVelY]) => {
+    let [velX, velY] = [initialVelX, initialVelY];
+    let [x, y] = [startX, startY];
+    let isBelowTarget = y < Math.min(y1, y2);
+    let isBeyondTarget = x > Math.max(x1, x2);
+    while (!isBelowTarget && !isBeyondTarget) {
+      x += velX;
+      y += velY;
+      velX = velX === 0 ? 0 : velX > 0 ? velX - 1 : velX + 1;
+      velY += -1;
+      isBelowTarget = y < Math.min(y1, y2);
+      isBeyondTarget = x > Math.max(x1, x2);
+
+      if (
+        x < Math.max(x1, x2) &&
+        x > Math.min(x1, x2) &&
+        y < Math.max(y1, y2) &&
+        y > Math.min(y1, y2)
+      ) {
+        // within target
+        return true;
+      }
     }
+    return false;
+  };
+  const trajectories = [];
+
+  if (willHitTarget([7, 2])) {
+    trajectories.push([7, 2]);
   }
+
+  // for (let x = -100; x <= 100; x++) {
+  //   for (let y = -100; y <= 100; y++) {
+  //     //
+  //   }
+  // }
+
   return null;
 };
