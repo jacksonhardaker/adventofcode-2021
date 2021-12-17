4ddd83084b14941d65c7193dcd5fb99f56befb69
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-17 09:30:21 -0500

    fix input parsing

--- apps/day-17/src/main.ts
+++ apps/day-17/src/main.ts
@@ -2,7 +2,7 @@ import { processInput } from '@adventofcode-2021/util-io';
 import { parseInput, trickShot } from './trickShot';
 
 (async () => {
-  const input = parseInput(await processInput('assets/input.txt', { root: __dirname })[0]);
+  const input = parseInput((await processInput('assets/input.txt', { root: __dirname }))[0]);
   const result1 = trickShot(input);
 
   console.log(`part 1: ${result1}`);
