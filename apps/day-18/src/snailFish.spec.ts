0b506e6629ac68bc61eb578ff0fb6f5aba9bc517
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-18 08:23:32 -0500

    add explode function

--- apps/day-18/src/snailFish.spec.ts
+++ apps/day-18/src/snailFish.spec.ts
@@ -1,8 +1,21 @@
-import { snailFish } from './snailFish';
+import { snailFish, explode, parseNumber } from './snailFish';
+
+describe('explode', () => {
+  test.each([
+    [
+      [[[[[9, 8], 1], 2], 3], 4],
+      [[[[0, 9], 2], 3], 4],
+    ] as Array<(number | number[])[]>,
+  ])('should explode the snailnumber as expected', (input, expected) => {
+    const result = explode(parseNumber(input));
+    console.log(JSON.stringify(result.root.toArray()));
+    expect(result.root.toArray()).toEqual(expected);
+  });
+});
 
 describe('snailFish', () => {
   test('should return the expected result', () => {
-    const input = [];
+    const input = [[[[[9, 8], 1], 2], 3], 4];
     const result = snailFish(input);
 
     expect(result).toEqual(null);
--- apps/day-18/src/snailFish.ts
+++ apps/day-18/src/snailFish.ts
@@ -1,3 +1,83 @@
+// export type SnailNumber = [number | SnailNumber, number | SnailNumber];
+
+class SnailNumber {
+  left: number | SnailNumber;
+  right: number | SnailNumber;
+  parent: null | SnailNumber;
+  depth: number;
+  constructor(
+    left: number | number[],
+    right: number | number[],
+    depth: number,
+    tails?: Set<SnailNumber>
+  ) {
+    this.left = Array.isArray(left)
+      ? new SnailNumber(left[0], left[1], depth + 1, tails)
+      : left;
+    this.right = Array.isArray(right)
+      ? new SnailNumber(right[0], right[1], depth + 1, tails)
+      : right;
+    this.depth = depth;
+
+    if (this.left instanceof SnailNumber) {
+      this.left.parent = this;
+    }
+    if (this.right instanceof SnailNumber) {
+      this.right.parent = this;
+    }
+
+    if (this.depth === 4) {
+      tails?.add?.(this);
+    }
+  }
+
+  update(left: number | SnailNumber, right: number | SnailNumber) {
+    Object.assign(this, { left, right });
+  }
+
+  closestLeft() {
+    return this.parent.left === this ? null : this.parent.left;
+  }
+  closestRight() {
+    return this.parent.right === this ? null : this.parent.right;
+  }
+
+  toArray() {
+    return [
+      this.left instanceof SnailNumber ? this.left.toArray() : this.left,
+      this.right instanceof SnailNumber ? this.right.toArray() : this.right,
+    ];
+  }
+}
+export const parseNumber = (input: Array<number | number[]>) => {
+  const [left, right] = input;
+  const tails = new Set<SnailNumber>();
+  const root = new SnailNumber(left, right, 0, tails);
+  return { root, tails };
+};
+
+export const explode = ({
+  root,
+  tails,
+}: {
+  root: SnailNumber;
+  tails: Set<SnailNumber>;
+}) => {
+  const [tail] = tails;
+  const closestLeft = tail.closestLeft() as number | null;
+  const closestRight = tail.closestRight() as number | null;
+  const left = closestLeft === null ? 0 : closestLeft + (tail.left as number);
+  const right =
+    closestRight === null ? 0 : closestRight + (tail.right as number);
+
+  tail.parent.update(left, right);
+
+  tails.delete(tail);
+
+  return { root, tails };
+};
+
 export const snailFish = (input: any[]) => {
+  parseNumber(input);
   return null;
 };
