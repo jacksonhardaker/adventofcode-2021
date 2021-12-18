3f2f83644df1b03176a9c6e578420055a82643cb
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-18 08:26:54 -0500

    add more test cases

--- apps/day-18/src/snailFish.spec.ts
+++ apps/day-18/src/snailFish.spec.ts
@@ -5,12 +5,40 @@ describe('explode', () => {
     [
       [[[[[9, 8], 1], 2], 3], 4],
       [[[[0, 9], 2], 3], 4],
-    ] as Array<(number | number[])[]>,
-  ])('should explode the snailnumber as expected', (input, expected) => {
-    const result = explode(parseNumber(input));
-    console.log(JSON.stringify(result.root.toArray()));
-    expect(result.root.toArray()).toEqual(expected);
-  });
+    ],
+    [
+      [7, [6, [5, [4, [3, 2]]]]],
+      [7, [6, [5, [7, 0]]]],
+    ],
+    [
+      [[6, [5, [4, [3, 2]]]], 1],
+      [[6, [5, [7, 0]]], 3],
+    ],
+    [
+      [3, [2, [1, [7, 3]]]],
+      [6, [5, [4, [3, 2]]]],
+    ],
+    [
+      [3, [2, [8, 0]]],
+      [9, [5, [4, [3, 2]]]],
+    ],
+    [
+      [
+        [3, [2, [8, 0]]],
+        [9, [5, [4, [3, 2]]]],
+      ],
+      [
+        [3, [2, [8, 0]]],
+        [9, [5, [7, 0]]],
+      ],
+    ],
+  ] as (number | number[])[][][])(
+    'should explode the snailnumber as expected',
+    (input, expected) => {
+      const result = explode(parseNumber(input));
+      expect(result.root.toArray()).toEqual(expected);
+    }
+  );
 });
 
 describe('snailFish', () => {
