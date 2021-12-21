33d05ea0696bda14a278dc7e2878840368f14c96
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-12-21 07:09:02 -0500

    complete for sample input

--- apps/day-21/src/diracDice.spec.ts
+++ apps/day-21/src/diracDice.spec.ts
@@ -2,9 +2,12 @@ import { diracDice } from './diracDice';
 
 describe('diracDice', () => {
   test('should return the expected result', () => {
-    const input = [];
+    const input = [
+      'Player 1 starting position: 4',
+      'Player 2 starting position: 8',
+    ];
     const result = diracDice(input);
 
-    expect(result).toEqual(null);
+    expect(result).toEqual(739785);
   });
 });
--- apps/day-21/src/diracDice.ts
+++ apps/day-21/src/diracDice.ts
@@ -1,3 +1,48 @@
-export const diracDice = (input: any[]) => {
-  return null;
+const parseInput = (input: string[]) =>
+  input.map((player) => Number(player.split(':').pop()));
+
+function* dieGenerator(): Generator<number, number, unknown> {
+  let side = 1;
+
+  while (true) {
+    yield side;
+    side++;
+    if (side > 100) {
+      side = 1;
+    }
+  }
+}
+
+export const diracDice = (input: string[]) => {
+  const [p1Start, p2Start] = parseInput(input);
+  const die = dieGenerator();
+  const scores = { p1: 0, p2: 0 };
+  const pos = { p1: p1Start, p2: p2Start };
+  let rolls = 0;
+  let turn = 'p1';
+
+  const switchPlayer = () => {
+    turn = turn === 'p1' ? 'p2' : 'p1';
+  };
+
+  const moveForward = (places) => {
+    const newPos = (pos[turn] + places) % 10;
+    pos[turn] = newPos === 0 ? 10 : newPos;
+    scores[turn] += pos[turn];
+  };
+
+  const roll = () => {
+    rolls++;
+    return die.next().value;
+  };
+
+  while (scores.p1 < 1000 && scores.p2 < 1000) {
+    const [roll1, roll2, roll3] = Array(3).fill(0).map(roll);
+
+    moveForward(roll1 + roll2 + roll3);
+
+    switchPlayer();
+  }
+
+  return Math.min(scores.p1, scores.p2) * rolls;
 };
